570c009e9bfe2b3b900048355d8d27adf55b5e95
johnpaulwelsh <wearing2raincoats@yahoo.com>
2013-11-03 15:09:28 -0500

    changed the regex in the-interrobanger.js

--- Firefox/chrome/content/the-interrobanger.js
+++ Firefox/chrome/content/the-interrobanger.js
@@ -1,46 +1,41 @@
-function() {
+walk(document.body);
 
-    function walk(node) {
-        // I stole this function and modified it from here:
-        // http://www.github.com/DaveRandom/cloud-to-butt-mozilla/
-        // who stole it from here: 
+function walk(node)
+{
+        // I stole this whole thing from:
+        // http://is.gd/WHhTDE
+        // The original creator stole this function from here:
         // http://is.gd/mwZp7E
-    
+        
         var child, next;
-    
-        switch ( node.nodeType ) {
-            case 1: // Element
-            case 9: // Document
-            case 11: // Document fragment
-                child = node.firstChild;
-                while ( child ) {
-                    next = child.nextSibling;
-                    walk(child);
-                    child = next;
-                }
-                break;
-    
-            case 3: // Text node
-                handleText(node);
-                break;
+
+        switch ( node.nodeType )
+        {
+                case 1: // Element
+                    if(node.value !== undefined)
+                        node.value = node.value.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+                case 9: // Document
+                case 11: // Document fragment
+                        child = node.firstChild;
+                        while ( child )
+                        {
+                                next = child.nextSibling;
+                                walk(child);
+                                child = next;
+                        }
+                        break;
+
+                case 3: // Text node
+                        handleText(node);
+                        break;
         }
-    }
-    
-    function handleText(textNode) {
+}
+
+function handleText(textNode)
+{
         var v = textNode.nodeValue;
-    
-        v = v.replace("What", "‽");
-    
+
+        v = v.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+
         textNode.nodeValue = v;
-    }
-
-    function windowLoadHandler() {
-        window.removeEventListener('load', windowLoadHandler);
-
-        document.getElementById('appcontent').addEventListener('DOMContentLoaded', function(e) {
-            walk(e.originalTarget.body);
-        });
-    }
-
-    window.addEventListener('load', windowLoadHandler);
-}());
+}

4326a207fc87d5a7e776384f0742c036300ea5bc
larouxn <larouxn@gmail.com>
2013-11-03 15:12:49 -0500

    Version 1.0 is complete for Opera, auto-replacement is fully integrated

--- Opera/content_script.js
+++ Opera/content_script.js
@@ -1,42 +1,42 @@
-window.addEventListener('load', function(event) {
-    function walk(node) 
-    {
-      	// I stole this whole thing from:
-      	// http://is.gd/V4x2D2
-        // I stole this function from here:
-        // http://is.gd/mwZp7E
+walk(document.body);
 
-        var child, next;
+function walk(node) 
+{
+	// I stole this whole thing from:
+  // http://is.gd/V4x2D2
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+	
+	var child, next;
 
-        switch ( node.nodeType )  
-        {
-            case 1:  // Element
-              if(node.value !== undefined)
-                node.value = node.value.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
-            case 9:  // Document
-            case 11: // Document fragment
-                child = node.firstChild;
-                while ( child ) 
-                {
-                    next = child.nextSibling;
-                    walk(child);
-                    child = next;
-                }
-                break;
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+      if(node.value !== undefined)
+        node.value = node.value.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
 
-            case 3: // Text node
-                handleText(node);
-                break;
-        }
-    }
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
 
-    function handleText(textNode) 
-    {
-      var v = textNode.nodeValue;
+function handleText(textNode) 
+{
+        var v = textNode.nodeValue;
 
-      v = v.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+        v = v.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
 
-      textNode.nodeValue = v;
-    }
+        textNode.nodeValue = v;
+}
 
-}, false);

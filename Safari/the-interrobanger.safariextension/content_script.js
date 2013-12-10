9546513c57825fc231c038de9f71a3c2aa990b6c
larouxn <larouxn@gmail.com>
2013-12-10 10:55:13 -0500

    Fixed error causing bug for Safari extension, version 1.1, new build!

--- Safari/the-interrobanger.safariextension/content_script.js
+++ Safari/the-interrobanger.safariextension/content_script.js
@@ -1,42 +1,37 @@
+function handleText(textNode) {
+    "use strict";
+    var v = textNode.nodeValue;
+    v = v.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+    textNode.nodeValue = v;
+}
+
+function walk(node) {
+    "use strict";
+    // I stole this whole thing from:
+    // http://is.gd/WHhTDE
+    // The original creator stole this function from here:
+    // http://is.gd/mwZp7E
+    var child, next;
+    switch (node.nodeType) {
+    case 1:
+        // Element
+        if (typeof node.value !== 'undefined') {
+            node.value = String(node.value).replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
+        }
+    case 11:
+        // Document fragment
+        child = node.firstChild;
+        while (child) {
+            next = child.nextSibling;
+            walk(child);
+            child = next;
+        }
+        break;
+    case 3:
+        // Text node
+        handleText(node);
+        break;
+    }
+}
+
 walk(document.body);
-
-function walk(node) 
-{
-	// I stole this whole thing from:
-	// http://is.gd/WHhTDE
-	// The original creator stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-      if(node.value !== undefined)
-        node.value = node.value.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
-
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
-
-function handleText(textNode) 
-{
-        var v = textNode.nodeValue;
-
-        v = v.replace(/(((\?+!+)|(!+\?+))[\?!]*)+/g, "‽");
-
-        textNode.nodeValue = v;
-}
-
